8c55ab95e1ddd6b93a177d9a20fa20c1bf0799ea
yossef-khaled <yossef.k.y.333@gmail.com>
2021-07-21 05:09:32 +0200

    Simple App.js component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import Main from './Components/MainComponent/Main';
+import { BrowserRouter } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
+
+  const [backgroundImage, setBackgroundImage] = useState('black-white-horizontally.jpg');
+
+  console.log(`${JSON.stringify(setBackgroundImage)} is the set function of the background image at App`);
+
   return (
-    <div className="App">
-      <Main/>
-    </div>
+    <BrowserRouter>
+        <div className="App" style=
+        {{minHeight: "100%", 
+          backgroundImage: `url(./images/${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "cover cover"
+        }}>
+          <Main onProjectsRender={(imageName) => setBackgroundImage(imageName)}/>
+        </div>
+    </BrowserRouter>
   );
 }
 
