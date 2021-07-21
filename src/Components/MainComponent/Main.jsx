e2c5e1e947542b9e058fe5d1d363084c4aa72a52
yossef-khaled <yossef.k.y.333@gmail.com>
2021-07-21 05:10:39 +0200

    Make the main component route throigh components.

--- src/Components/MainComponent/Main.jsx
+++ src/Components/MainComponent/Main.jsx
@@ -1,42 +1,25 @@
 import React from 'react';
-import './style.css';
-import Avatar from '@material-ui/core/Avatar';
-import { PropTypes } from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Home from '../HomeCopmonent/Home';
 import Header from '../HeaderComponent/Header';
+import Projects from '../ProjectsComponent/Projects';
 
-const styles = {
-    myPhoto: {
-        marginTop: "10%",
-        marginLeft: "40%",
-        height: "15rem",
-        width: "15rem"
-    },
-    blacked: {
-        color: "#000000"
-    }
-};
 
-function Main(props) {
-    
-    const { classes } = props; 
+function Main(props) { 
 
-    return(
-        <div>
-            <Header color="secondary"/>
-            <h2>𝕄𝕪 𝕟𝕒𝕞𝕖 𝕚𝕤 𝕐𝕠𝕦𝕤𝕤𝕖𝕗 𝕂𝕙𝕒𝕝𝕖𝕕, 𝕀 𝕒𝕞 𝕒 𝕨𝕖𝕓 𝕕𝕖𝕧𝕖𝕝𝕠𝕡𝕖𝕣.</h2>
-            <Avatar alt="Photo of me" src="../../images/purple-space-background-cartoon.jpg" className={classes.myPhoto}/>
-            <h3 className={classes.blacked}> - Most of the time I play football , in my free times I do some "typin in keyboard" stuff.</h3>
-            <br/>
-            <h3 className={classes.blacked} >First computer I saw was in : 26-10-2018</h3>
-        </div>
-        
+
+    console.log(`${JSON.stringify(props.setBackgroundImage)} at Main`);
+
+    return (
+        <>
+            <Switch>
+                <Route path="/home" component={() => <Home />}/>
+                <Route path="/projects" component={() => <Projects onProjectsRender={(imageName) => props.onProjectsRender(imageName)}/>}/>
+                <Redirect to="/home"/>
+            </Switch>
+        </>
     );
 }
 
 
-Main.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(Main);
+export default Main;
