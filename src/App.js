c7e04f4de15b965ff99f8e349ccfc257fed65fc9
yossef-khaled <yossef.k.y333@gmail.com>
2021-08-25 02:17:58 +0200

    Comment the image value.

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ function App() {
 
   const [backgroundImage, setBackgroundImage] = useState('black-white-horizontally.jpg');
 
-  console.log(`${JSON.stringify(setBackgroundImage)} is the set function of the background image at App`);
+  console.log(`${JSON.stringify(backgroundImage)} is the background image at App`);
 
   return (
     <BrowserRouter>
         <div className="App" style=
-        {{minHeight: "100%", 
-          backgroundImage: `url(./images/${backgroundImage})`,
+        {{
+          minHeight: "100%",
+          maxHeight: "500%", 
+          //backgroundImage: `url(../src/Shared/images/${backgroundImage})`,
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
           backgroundPosition: "cover cover"
