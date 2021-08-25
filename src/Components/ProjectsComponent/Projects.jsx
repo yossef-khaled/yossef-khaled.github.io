8ae4a7d8a3880ab335d44e8c46335e7f5f837931
yossef-khaled <yossef.k.y333@gmail.com>
2021-08-25 02:23:06 +0200

    Translate the projects data into a child.

--- src/Components/ProjectsComponent/Projects.jsx
+++ src/Components/ProjectsComponent/Projects.jsx
@@ -2,22 +2,44 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Header from '../HeaderComponent/Header';
+import PROJECTS_DATA from '../../Shared/Data/Projects';
+import Project from '../ProjectComponent/Project';
+import Container from '@material-ui/core/Container';
 
 const styles = {
-
+    horizontalLine: {
+        width: "100%",
+    }
 }
 
 function Projects(props) {
     
     const { classes } = props;
-    console.log(`${JSON.stringify(props)} at Projects`);
-    props.onProjectsRender('black-white-vertically.jpg');
-     
+    console.log(`${JSON.stringify(props)} at Projects`);    
+    //props.onProjectsRender('black-white-vertically.jpg');
+
+    const projects = PROJECTS_DATA.map((project) => {
+        return(
+            <>
+                <Project projectName={project.name}
+                         projectDescription={project.description}
+                         stack={project.stack}
+                         role={project.myRole}
+                         link={project.link}
+    
+                />
+                {project.id != PROJECTS_DATA.length && <hr className={classes.horizontalLine}/>}
+            </>
+        )
+    });
+    
     return(
         
         <div>
-            <Header color="#000000" borderColor="#000000"/>
-            <h1>Projects Component</h1>
+            <Header color="#000000" borderColor="#000000" backgroundColor="#ffffff"/>
+            <Container>
+                {projects}
+            </Container>
         </div>
     );
 
