9e2f9837a928089d60609a00be3260fc8f9e8f81
yossef-khaled <yossef.k.y333@gmail.com>
2021-08-25 02:22:05 +0200

    The projects data as a JSON array.

---
+++ src/Shared/Data/Projects.js
@@ -0,0 +1,53 @@
+const PROJECTS_DATA = [
+    {
+        id: 1,
+        name: "Canesee",
+        myRole: "Graduation project - Android developer & Hardware engineer.",
+        stack: "(TensorFlow, Android/kotlin, Raspberry Pi, Arduino).",
+        description: "Smart glasses and cane for the blind. Recoginses faces, texts, scenery, and emotions. Uses ultrasonic sensors to detect obstacles.",
+        link: "https://github.com/canesee-project"
+    },
+    {
+        id: 2,
+        name: "Open Learning Quran Platform",
+        myRole: "Frontend developer.",
+        stack: "(React, Flask, PostgreSQL).",
+        description: "Online platform for remotely teaching and reciting the Quran.",
+        link: "https://github.com/Open-Quran-Learning"
+    },
+    {
+        id: 3,
+        name: "Menu App (Frontend)",
+        myRole: "Frontend Developer",
+        stack: "(React : Redux, Redux-forms, Reactstrap, Actions & reducers).",
+        description: "A website of a restaurant with user authentication & user feedback.",
+        link: "https://github.com/yossef-khaled/Menu-App"
+    },
+    {
+        id: 4,
+        name: "Menu App (Backend)",
+        myRole: "Backend Developer",
+        stack: "(NodeJS : Express, Express-router, Body-parser, Morgan & MongoDB).",
+        description: "The backend implementation for the previous project.",
+        link: "https://github.com/yossef-khaled/Menu-app-backend"
+    },
+    {
+        id: 5,
+        name: "Weather Journal App",
+        myRole: "Backend Developer",
+        stack: "(NodeJS : Express).",
+        description: "A web App that uses OWM (Open Weather Map) API to post the temperature of any city you choose using its zip code or name.",
+        link: "https://github.com/yossef-khaled/Weather-Journal-web"
+    },
+    {
+        id: 6,
+        name: "Arithmetic Expression Evaluator",
+        myRole: null,
+        stack: "C# Language",
+        description: "A parser for the arithmetic expressions using C# language.",
+        link: "https://github.com/yossef-khaled/Arithmetic-expressions-evaluator"
+    },
+
+];
+
+export default PROJECTS_DATA;
