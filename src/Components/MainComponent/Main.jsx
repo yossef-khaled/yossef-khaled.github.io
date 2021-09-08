fb529fd91903a7493f07c2d5107bb81df7e1c103
yossef-khaled <yossef.k.y333@gmail.com>
2021-09-08 23:31:15 +0200

    Add education to the route

--- src/Components/MainComponent/Main.jsx
+++ src/Components/MainComponent/Main.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from '../HomeCopmonent/Home';
-import Header from '../HeaderComponent/Header';
 import Projects from '../ProjectsComponent/Projects';
-
+import Education from '../EducationComponent/Education';
 
 function Main(props) { 
 
-
     console.log(`${JSON.stringify(props.onComponentRender())} at Main`);
-    
 
     return (
         <>
             <Switch>
                 <Route path="/home" component={() => <Home onHomeRender={(imageName) => props.onComponentRender(imageName)}/>}/>
                 <Route path="/projects" component={() => <Projects onProjectsRender={(imageName) => props.onComponentRender(imageName)}/>}/>
+                <Route path="/education" component={() => <Education onEducationRender={(imageName) => props.onComponentRender(imageName)}/>}/>
                 <Redirect to="/home"/>
             </Switch>
         </>
