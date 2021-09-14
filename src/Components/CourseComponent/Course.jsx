7a291b2247827cfe466e546fee9c44ad2be24714
yossef-khaled <yossef.k.y333@gmail.com>
2021-09-15 00:34:22 +0200

    Add course component

---
+++ src/Components/CourseComponent/Course.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+
+function Course(props) {
+    return(
+        <Container>
+            
+        </Container>
+    );
+}
+
+export default Course;
