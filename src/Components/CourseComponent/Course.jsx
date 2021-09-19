dce0f49de325da6418bb9c5ae63729b1025f9183
yossef-khaled <yossef.k.y333@gmail.com>
2021-09-19 22:42:40 +0200

    Add a component to display the course data.

--- src/Components/CourseComponent/Course.jsx
+++ src/Components/CourseComponent/Course.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
 import Container from '@material-ui/core/Container';
+import { PropTypes } from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+    mainContainer: {
+        color: "#000000",
+        textAlign: "right",
+        marginLeft: "10%",
+    }
+};
 
 function Course(props) {
+  
+    const { classes } = props;
+  
     return(
-        <Container>
-            
+        <Container className={classes.mainContainer}>
+            <Typography variant="overline">
+                {props.courseName}
+            </Typography>
         </Container>
     );
 }
 
-export default Course;
+
+Course.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Course);
