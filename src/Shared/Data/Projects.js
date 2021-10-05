075027e69cf1b43dce1ba1544d945b87992a5a3e
yossef-khaled <yossef.k.y333@gmail.com>
2021-10-05 23:04:17 +0200

    Assign each project a type.

--- src/Shared/Data/Projects.js
+++ src/Shared/Data/Projects.js
@@ -3,6 +3,7 @@ const PROJECTS_DATA = [
         id: 1,
         name: "Canesee",
         myRole: "Android developer & Hardware engineer. (Graduation project)",
+        type: "graduation project  2019-2020",
         stack: "(TensorFlow, Android/kotlin, Raspberry Pi, Arduino).",
         description: "Smart glasses and cane for the blind. Recoginses faces, texts, scenery, and emotions. Uses ultrasonic sensors to detect obstacles.",
         link: "https://github.com/canesee-project"
@@ -11,6 +12,7 @@ const PROJECTS_DATA = [
         id: 2,
         name: "Open Learning Quran Platform",
         myRole: "Frontend developer.",
+        type: "school project  2019-2020",
         stack: "(React, Flask, PostgreSQL).",
         description: "Online platform for remotely teaching and reciting the Quran.",
         link: "https://github.com/Open-Quran-Learning"
@@ -19,6 +21,7 @@ const PROJECTS_DATA = [
         id: 3,
         name: "Menu App (Frontend)",
         myRole: "Frontend Developer",
+        type: "course project",
         stack: "(React : Redux, Redux-forms, Reactstrap, Actions & reducers).",
         description: "A website of a restaurant with user authentication & user feedback.",
         link: "https://github.com/yossef-khaled/Menu-App"
@@ -27,6 +30,7 @@ const PROJECTS_DATA = [
         id: 4,
         name: "Menu App (Backend)",
         myRole: "Backend Developer",
+        type: "course project",
         stack: "(NodeJS : Express, Express-router, Body-parser, Morgan & MongoDB).",
         description: "The backend implementation for the previous project.",
         link: "https://github.com/yossef-khaled/Menu-app-backend"
@@ -35,6 +39,7 @@ const PROJECTS_DATA = [
         id: 5,
         name: "Weather Journal App",
         myRole: "Backend Developer",
+        type: "course project",
         stack: "(NodeJS : Express).",
         description: "A web App that uses OWM (Open Weather Map) API to post the temperature of any city you choose using its zip code or name.",
         link: "https://github.com/yossef-khaled/Weather-Journal-web"
@@ -43,9 +48,37 @@ const PROJECTS_DATA = [
         id: 6,
         name: "Arithmetic Expression Evaluator",
         myRole: null,
+        type: "school project",
         stack: "C# Language",
         description: "A parser for the arithmetic expressions using C# language.",
         link: "https://github.com/yossef-khaled/Arithmetic-expressions-evaluator"
+    },
+    {
+        id: 7,
+        name: "Online Book Store",
+        myRole: 'Backend Developer',
+        type: "school project - 2019-2020",
+        stack: ".Net Core",
+        description: "An online book store where you can buy or sell any book.",
+        link: "https://github.com/yossef-khaled/OnlineBookStore"
+    },
+    {
+        id: 8,
+        name: "LC-3 (Little Computer 3)",
+        myRole: null,
+        type: "school project - 2018-2019",
+        stack: "Python language",
+        description: "A type of computer educational programming language, an assembly language, which is a type of low-level programming language.",
+        link: "https://github.com/yossef-khaled/LC-3"
+    },
+    {
+        id: 9,
+        name: "Node-js IOT",
+        myRole: 'Hardware Engineer',
+        type: "school project - 2018-2019",
+        stack: "JS, HTML & CSS for the front. Node MCU for the HW.",
+        description: "IOT project with NodeMCU, Express, Websockets ",
+        link: "https://github.com/yossef-khaled/nodejs-iot"
     },
 
 ];
