09f23511ce7d2c20cec5a7cdf6937aa90818336f
yossef-khaled <yossef.k.y333@gmail.com>
2021-10-05 02:19:29 +0200

    Pass the certificates as image objects.

--- src/Shared/Data/Courses.js
+++ src/Shared/Data/Courses.js
@@ -1,12 +1,24 @@
+//Import organizations logos 
 import ITI from '../../Shared/images/ITI.png';
 import Coursera from '../../Shared/images/Coursera.png';
 import Udacity from '../../Shared/images/Udacity.png';
+
+//Import courses certificates
+import itiCertificate from '../Data/Certificates/ITI_Certificate.PNG';
+import specializationCertificate from '../Data/Certificates/Web_Specialization_Certificate.PNG';
+import htmlCertificate from '../Data/Certificates/HTML_Certificate.PNG';
+import cssCertificate from '../Data/Certificates/CSS_Certificate.PNG';
+import jsCertificate from '../Data/Certificates/JavaScript_Certificate.PNG';
+import responsiveDesignCertificate from '../Data/Certificates/Responsive_Design_Certificate.PNG';
+import capstoneCertificate from '../Data/Certificates/Capstone_Certificate.PNG';
+import nanoDegreeCertificate from '../Data/Certificates/Nano_degree_certificate.JPG';
 
 const COURSES_DATA = [
     {
         id: 0,
         name: '.NET Web Development',
-        organization: ITI,
+        organization: 'ITI',
+        organizationImg: ITI,
         date: 'Aug 2019 - Sep 2019',
         duration: '2 Months',
         type: 'course',
@@ -17,81 +29,88 @@ const COURSES_DATA = [
             'LINQ and Entity Framework.'
         ],
         credentialId: null,
-        certificate: 'ITI_Certificate.PNG',
+        certificate: itiCertificate,
     },
     {
         id: 1,
         name: 'Web Design for Everybody: Basics of Web Development & Coding Specialization',
-        organization: Coursera,
+        organization: 'Coursera',
+        organizationImg: Coursera,
         date: 'Feb 2020 - Apr 2020',
         duration: '3 Months',
-        details: 'A Specialization Of Front End Web Development. It Consists Of 4 Courses And A Capstone.',
+        details: 'A Specialization Of Front End Web Development. It Consists Of the following 4 Courses And A Capstone.',
         type: 'specialization',
         courses: 
             [
                 {
                     id: 0,
                     name: 'Introducrtion To HTML',
-                    organization: Coursera,
+                    organization: 'Coursera',
+                    organizationImg: Coursera,
                     date: 'Feb 2020',
                     duration: '2 weeks',
                     details: 'A brief introduction into HTML.',
                     type: 'course',
                     credentialId: '538CNR3FLQ9W',
-                    certificate: 'HTML_Certificate.PNG',
+                    certificate: htmlCertificate,
                 },
                 {
                     id: 1,
                     name: 'Introduction to CSS3',
-                    organization: Coursera,
+                    organization: 'Coursera',
+                    organizationImg: Coursera,
                     date: 'Feb 2020',
                     duration: '2 Weeks',
                     details: 'A brief introduction into CSS3.',
                     type: 'course',
                     credentialId: '96RZDDCNP64L',
-                    certificate: 'CSS_Certificate.PNG',
+                    certificate: cssCertificate,
                 },
                 {
                     id: 2,
                     name: 'Interactivity With JavaScript',
-                    organization: Coursera,
+                    organization: 'Coursera',
+                    organizationImg: Coursera,
                     date: 'Mar 2020',
                     duration: '1 month',
                     details: 'Make A web page more dynamic and handle user inputs using javascript.',
                     type: 'course',
                     credentialId: 'S6UT3LJT93HL',
-                    certificate: 'JavaScript_Certificate.PNG',
+                    certificate: jsCertificate,
                 },
                 {
                     id: 3,
                     name: 'Advanced Styling with Responsive Design',
-                    organization: Coursera,
+                    organization: 'Coursera',
+                    organizationImg: Coursera,
                     date: 'Apr 2020',
                     duration: '2 weeks',
                     details: 'Make a web page responsive to the size of the screen the user is using.',
                     type: 'course',
                     credentialId: 'VY7HM8J3THYN',
-                    certificate: 'Responsive_Design_Certificate.PNG',
+                    certificate: responsiveDesignCertificate,
                 },
                 {
                     id: 4,
                     name: 'Web Design for Everybody Capstone',
-                    organization: Coursera,
+                    organization: 'Coursera',
+                    organizationImg: Coursera,
                     date: 'Apr 2020',
                     duration: '2 Weeks',
                     details: 'Capstone for the course.',
                     type: 'course',
                     credentialId: 'DV3W8U7W3J4V',
-                    certificate: 'Capstone_Certificate.PNG',
+                    certificate: capstoneCertificate,
                 },
             ],
         credentialId: 'TV7NZ6XY7P72',
-        certificate: 'Web_Specialization_Certificate.PNG'
+        certificate: specializationCertificate
     },
     {
         id: 7,
         name: 'Server-side Development with NodeJS, Express and MongoDB',
-        organization: Coursera,
+        organization: 'Coursera',
+        organizationImg: Coursera,
         date: 'Sep 2020',
         duration: '1 Month',
         details: 'Learn REST API with express, body-parser, & morgan modules, Express-router to route, Mongoose module for the database.',
@@ -102,18 +121,20 @@ const COURSES_DATA = [
     {
         id: 8,
         name: 'Web Development Nano Degree Professional Track',
-        organization: Udacity,
+        organization: 'Udacity',
+        organizationImg: Udacity,
         date: 'July 2020 - Aug 2020',
         duration: '6 Weeks',
         details: 'Learn Advanced Javascript, DOM Methods & Working With APIs.',
         type: 'course',
         credentialId: null,
-        certificate: 'Nano_degree_certificate.JPG',
+        certificate: nanoDegreeCertificate,
     },
     {
         id: 9,
         name: 'Front-End Web Development With React',
-        organization: Coursera,
+        organization: 'Coursera',
+        organizationImg: Coursera,
         date: 'Oct 2020 - May 2021',
         duration: '7 Months (Too much Due To My Military Service)',
         details: 'Learn How To Build Responsive, Interactive With The Back-end, Friendly UI/UX View With React.',
